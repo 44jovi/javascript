91a71c6ab20cf181022ea0d46171fe92398b7e16
jovi <kwong.jovi@gmail.com>
2022-11-28 23:58:46 +0000

    add snippet x 3

--- snippets/output-quizzes.js
+++ snippets/output-quizzes.js
@@ -1,2 +1,27 @@
 // All snippets commented out by default.
 // Uncomment specific snippets and view output using node.
+// Snippets currently in no particular order.
+
+// // - - - -
+// console.log(typeof Array);
+// // Output: 'function'
+
+// // - - - -
+// const getArgsLen = (a, b, c) => {
+//   console.log(getArgsLen.length);
+//   console.log(arguments.length);
+// };
+// // Output: '5'(?)
+
+// // - - - -
+// const arr1 = new Array(10, 9, 8);
+// console.log(arr1);
+// // Output: '[ 10, 9, 8 ]'
+// console.log(arr1.length);
+// // Output: '3'
+
+// const arr2 = new Array(10);
+// console.log(arr2);
+// // Output: '[ <10 empty items> ]'
+// console.log(arr2.length);
+// // Output: '10'
