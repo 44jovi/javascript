41c5cb5c87506dfde9abc67b6e1939970d6a3d0d
jovi <kwong.jovi@gmail.com>
2022-12-17 23:29:57 +0000

    Add snippets: promises

--- snippets/promises.js
+++ snippets/promises.js
@@ -1,4 +1,4 @@
-let somePromise = new Promise((resolve, reject) => {
+let myPromise = new Promise((resolve, reject) => {
   let x = false;
   if (x === true) {
     resolve("Successful promise!");
@@ -7,10 +7,81 @@ let somePromise = new Promise((resolve, reject) => {
   }
 });
 
-somePromise
+myPromise
   .then((resolveMessage) => {
     console.log("Resolved: " + resolveMessage);
   })
   .catch((rejectMessage) => {
     console.log("Rejected: " + rejectMessage);
   });
+// => Rejected: Failed promise!
+
+// - - - - - - - -
+
+// Example: converting a callback into a promise
+
+let someError = true;
+
+const someFunc = (cb, errorCb) => {
+  if (someError) {
+    errorCb("Some error occured!");
+  } else {
+    cb("It worked!");
+  }
+};
+
+someFunc(
+  (successMesssage) => {
+    console.log("Success: " + successMesssage);
+  },
+  (errorMessage) => {
+    console.log("Error: " + errorMessage);
+  }
+);
+// => Error: Some error occured!
+
+someError = false;
+
+someFunc(
+  (successMesssage) => {
+    console.log("Success: " + successMesssage);
+  },
+  (errorMessage) => {
+    console.log("Error: " + errorMessage);
+  }
+);
+// => Success: It worked
+
+// Convert to a promise
+
+const somePromise = () => {
+  return new Promise((resolve, reject) => {
+    if (someError) {
+      resolve("Some error occured!");
+    } else {
+      reject("It worked!");
+    }
+  });
+};
+
+someError = true;
+
+somePromise()
+  .then((errorMessage) => {
+    console.log("Promise error: " + errorMessage);
+  })
+  .catch((successMesssage) => {
+    console.log("Promise success: " + successMesssage);
+  });
+// => Promise error: Some error occured!
+
+someError = false;
+
+somePromise()
+  .then((errorMessage) => {
+    console.log("Promise error: " + errorMessage);
+  })
+  .catch((successMesssage) => {
+    console.log("Promise success: " + successMesssage);
+  });
+// => Promise success: It worked!
