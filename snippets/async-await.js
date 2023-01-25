0c45161657fadb670c1e3dd92faf9afc9dd0c39b
jovi <kwong.jovi@gmail.com>
2023-01-25 20:48:31 +0000

    feat(async-await.js): add snippet for basic 'await' example

--- snippets/async-await.js
+++ snippets/async-await.js
@@ -1,12 +1,26 @@
 // 'async' always returns a promise even without 'await'
-async function someFunc() {
+async function someFunc1() {
   return 1234;
   // same as explicitly returning a promise:
   // return Promise.resolve(1234);
 }
 
-someFunc().then((response) => {
+someFunc1().then((response) => {
   console.log(response);
 });
 
 // => 1234
+
+// 'await' waits until a promise is fulfilled before it returns its result
+
+async function someFunc2() {
+  let promise = new Promise((resolve) => {
+    setTimeout(() => resolve("Promise resolved after waiting 1000ms!"), 1000);
+  });
+
+  let resposne = await promise;
+
+  console.log(resposne);
+}
+
+someFunc2();
